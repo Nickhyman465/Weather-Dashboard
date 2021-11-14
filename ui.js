b608d042134c0239808e55245956fedc0487f54d
Nickhyman465 <nickhyman465@gmail.com>
2021-11-14 12:57:29 -0500

    added local storage functionality

--- ui.js
+++ ui.js
@@ -16,4 +16,27 @@ class UI {
         </div>
         `;
     }
+
+    clearUI() {
+        uiContainer.innerHTML = "";
+      }
+    
+      saveToLS(data) {
+        localStorage.setItem("city", JSON.stringify(data));
+      }
+    
+      getFromLS() {
+        if (localStorage.getItem("city" == null)) {
+          return this.defaultCity;
+        } else {
+          this.city = JSON.parse(localStorage.getItem("city"));
+        }
+    
+        return this.city;
+      }
+    
+      clearLS() {
+        localStorage.clear();
+    }
 }
+
